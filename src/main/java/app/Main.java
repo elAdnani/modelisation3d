4b48f43f36f3bc8206e30f2c00c17fc797766318
Virgil Hazebroucq <virgil.hazebroucq.etu@univ-lille.fr>
2021-11-07 06:34:15 +0100

    Ajout de models, modification du main maven, suppr du demo FXML

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,14 @@
+package App;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import views.Affichage;
+
+public class Main extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		new Affichage().start(arg0);	
+	}
+
+}
