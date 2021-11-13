236bf8206e436510adbd66f734ad08aa41c4fdf6
Virgil Hazebroucq <virgil.hazebroucq.etu@univ-lille.fr>
2021-11-13 05:13:24 +0100

    Imports

--- src/test/java/modele/MatriceTest.java
+++ src/test/java/modele/MatriceTest.java
@@ -1,6 +1,5 @@
 package modele;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
