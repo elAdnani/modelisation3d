b569b25d13444d95c074db040120a44eb96a4036
Adnan Kouakoua <adnan.kouakoua.etu@univ-lille.fr>
2021-12-13 18:47:37 +0100

    Plus de problème de lumière fixe

--- src/main/java/modele/Face.java
+++ src/main/java/modele/Face.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Face {
 
-	private List<Point> points;
-	private static final   Vecteur SOURCELUMINEUSE = new Vecteur(0, 0, 1);
-	private Vecteur normalUnitaire;
+	private List<Point>          points;
+	private static final Vecteur SOURCELUMINEUSE = new Vecteur(0, 0, 1);
+	private Vecteur              normalUnitaire;
 
 	// List<Integer> reference; // indice de la matrice des listes de point
 	// Matrice METTRE EN PARAMETRE
@@ -18,8 +18,9 @@ public class Face {
 
 	public Face(List<Point> points) {
 		this.points = points;
-		if(points.size()>=3) {
-			this.normalUnitaire = Vecteur.normalUnitaire(this.points.get(0),this.points.get(1),this.points.get(2));
+		if (points.size() >= 3)
+		{
+			this.normalUnitaire = Vecteur.normalUnitaire(this.points.get(0), this.points.get(1), this.points.get(2));
 		}
 	}
 
@@ -29,7 +30,8 @@ public class Face {
 
 	public String toString() {
 		StringBuilder sb = new StringBuilder("[Face");
-		for (Point p : points) {
+		for (Point p : points)
+		{
 			sb.append(p + "---");
 		}
 		sb.append("]");
@@ -38,7 +40,8 @@ public class Face {
 
 	public double getAverageZ() {
 		double sum = 0;
-		for (Point p : points) {
+		for (Point p : points)
+		{
 			sum += p.getZ();
 		}
 
@@ -47,7 +50,8 @@ public class Face {
 
 	public double getAverageY() {
 		double sum = 0;
-		for (Point p : points) {
+		for (Point p : points)
+		{
 			sum += p.getY();
 		}
 
@@ -56,48 +60,53 @@ public class Face {
 
 	public double getAverageX() {
 		double sum = 0;
-		for (Point p : points) {
+		for (Point p : points)
+		{
 			sum += p.getX();
 		}
 
 		return sum / points.size();
 	}
-	
-	
+
 	public double degreDeCouleur() {
-		if(this.points.size() >=3 && this.normalUnitaire == null) {
-			this.normalUnitaire = Vecteur.normalUnitaire(this.points.get(0),this.points.get(1),this.points.get(2));
-		}
-		double degreDeCouleur =	(double)(this.normalUnitaire.getX() * SOURCELUMINEUSE.getX()) 
-								+ (double)(this.normalUnitaire.getY() * SOURCELUMINEUSE.getY()) 
-								+ (double)(this.normalUnitaire.getZ() * SOURCELUMINEUSE.getZ());
-		if(degreDeCouleur<0) {
-			degreDeCouleur=0;
-		}
-		if(degreDeCouleur>255) {
-			degreDeCouleur=255;
+		double degreDeCouleur = -1;
+		if (this.points.size() >= 3)
+		{
+
+			this.normalUnitaire = Vecteur.normalUnitaire(this.points.get(0), this.points.get(1), this.points.get(2));
+
+			degreDeCouleur = (double) (this.normalUnitaire.getX() * SOURCELUMINEUSE.getX())
+					+ (double) (this.normalUnitaire.getY() * SOURCELUMINEUSE.getY())
+					+ (double) (this.normalUnitaire.getZ() * SOURCELUMINEUSE.getZ());
+			if (degreDeCouleur < 0)
+			{
+				degreDeCouleur = 0;
+			}
+			if (degreDeCouleur > 255)
+			{
+				degreDeCouleur = 255;
+			}
 		}
 
 		return degreDeCouleur;
-		
+
 	}
-	
-/*	public static void main(String[] args) {
-		
-		Face f = new Face();
-		f.add(new Point(-2, 1, 3));
-		f.add(new Point(-3, 2, 3));
-		f.add(new Point(-2, 2, 5));
-		
-		System.out.println(f.degreDeCouleur());
-	}*/
+
+	/*
+	 * public static void main(String[] args) {
+	 * 
+	 * Face f = new Face(); f.add(new Point(-2, 1, 3)); f.add(new Point(-3, 2, 3));
+	 * f.add(new Point(-2, 2, 5));
+	 * 
+	 * System.out.println(f.degreDeCouleur()); }
+	 */
 
 	public void add(Point p) {
 		this.points.add(p);
-		if(points.size()>=3) {
-			this.normalUnitaire = Vecteur.normalUnitaire(this.points.get(0),this.points.get(2),this.points.get(3));
+		if (points.size() >= 3)
+		{
+			this.normalUnitaire = Vecteur.normalUnitaire(this.points.get(0), this.points.get(2), this.points.get(3));
 		}
 	}
-
 
 }
---
+++ src/test/java/modele/VecteurTest.java
@@ -0,0 +1,30 @@
+package modele;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+/**
+ * 
+ * Cette classe regroupe les testes confirmant les fonctionnalités des méthodes
+ * réalisées dans {@link Matrice}. <br>
+ *
+ * @author <a href="mailto:adnan.kouakoua@univ-lille1.fr">Adnân KOUAKOUA</a>
+ *         IUT-A Informatique, Universite de Lille.
+ * @date 9 nov. 2021
+ */
+public class VecteurTest {
+
+	
+	@BeforeAll
+	public static void beforeAllTests() {
+		System.out.println("Début des tests pour la classe Matrice\n");
+
+	}
+
+	@AfterAll
+	public static void afterAllTests() {
+		System.out.println("Fin des tests de Matrice");
+
+	}
+	
+}
