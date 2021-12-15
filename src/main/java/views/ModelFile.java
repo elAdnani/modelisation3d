356b88053cdd926c311325500787845c04d23fdd
Sene Hugo <hugo.sene.etu@univ-lille.fr>
2021-12-15 10:24:54 +0100

    Search bar + auteur + date creation

---
+++ src/main/java/views/ModelFile.java
@@ -0,0 +1,15 @@
+package views;
+
+public class ModelFile {
+		private String name,author,faces,points,created;
+
+		public ModelFile(String name, String author, String faces, String points, String created) {
+			super();
+			this.name = name;
+			this.author = author;
+			this.faces = faces;
+			this.points = points;
+			this.created = created;
+		}
+
+}
