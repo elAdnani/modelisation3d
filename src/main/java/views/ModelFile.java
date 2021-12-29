8e21fe14cc2ae7a03097f0f00fded6c377c48851
Hugo Sene <hugo.sene.etu@univ-lille.fr>
2021-12-29 12:27:23 +0100

    Update ModelFile.java

--- src/main/java/views/ModelFile.java
+++ src/main/java/views/ModelFile.java
@@ -1,15 +1,78 @@
 package views;
 
-public class ModelFile {
-		private String name,author,faces,points,created;
 
-		public ModelFile(String name, String author, String faces, String points, String created) {
+
+
+
+public class ModelFile {
+		private String name,author,created,size;
+		private int faces,points;
+		private String path;
+		public ModelFile(String path,String name,String size,int faces, int points, String created,String author) {
 			super();
+			this.path = path;
 			this.name = name;
+			this.size = size;
 			this.author = author;
 			this.faces = faces;
 			this.points = points;
 			this.created = created;
+		}
+		
+		public String getName() {
+			return name;
+		}
+
+		public String getPath() {
+			return path;
+		}
+
+		public void setPath(String path) {
+			this.path = path;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getAuthor() {
+			return author;
+		}
+
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+
+		public String getCreated() {
+			return created;
+		}
+
+		public void setCreated(String created) {
+			this.created = created;
+		}
+
+		public String getSize() {
+			return size;
+		}
+
+		public void setSize(String size) {
+			this.size = size;
+		}
+
+		public int getFaces() {
+			return faces;
+		}
+
+		public void setFaces(int faces) {
+			this.faces = faces;
+		}
+
+		public int getPoints() {
+			return points;
+		}
+
+		public void setPoints(int points) {
+			this.points = points;
 		}
 
 }
